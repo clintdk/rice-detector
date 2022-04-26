2e3b0bbd78a583bb4bf6b938e79dd189eac52a56
Clint de Keizer <clintdekeizer@mbp-van-clint.kpn>
2022-04-26 11:35:07 +0200

    add the preparedata.py file and preparedata class with empty methods

---
+++ src/preparedata.py
@@ -0,0 +1,19 @@
+class preparedata:
+    def __init__(self, path: str):
+        self.path = path
+        self.__loaddata()
+        self.__normalizedata()
+        self.__resizedata()
+        self.__splitdata()
+
+    def __loaddata():
+        pass
+
+    def __normalizedata():
+        pass
+
+    def __resizedata():
+        pass
+
+    def __splitdata():
+        pass
